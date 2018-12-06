8fa661fd154e323de7b8d48ff1a9fb2e20f3e932
Nevin Zehr <nevinzehr1@gmail.com>
2018-12-05 23:46:17 -0500

    Replaced fixed text with labelText var

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -13,10 +13,12 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const buttonText = { text: 'Click Me' };
+    const labelText = 'Enter name:'
+
   return (
     <div>
       <label className="label" for="name">
-        Enter name:
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
