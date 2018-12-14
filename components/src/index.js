b07fc28b8b0b7f707550ad952bf3391c833df3ae
Nevin Zehr <nevinzehr1@gmail.com>
2018-12-13 21:46:04 -0500

    turned avatar src into prop, commented out import faker on CommentDetail.js

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-    <CommentDetail author="Sam" timeAgo="Today at 4:43pm" commentContent="Great blog post, dude!" />
-    <CommentDetail author="Alex" timeAgo="Today at 2:45pm" commentContent="Interesting and informative read." />
-    <CommentDetail author="Jane" timeAgo="Yesterday at 5:43am" commentContent="9/11 was an inside job." />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:43pm"
+        commentContent="Great blog post, dude!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:45pm"
+        commentContent="Interesting and informative read."
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:43am"
+        commentContent="9/11 was an inside job."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
