9e1f2f1bb8087fb56186d430133532f758013d2b
Nevin Zehr <nevinzehr1@gmail.com>
2018-12-13 21:09:24 -0500

    Replaced timeAgo and commentContent hardcode with variables

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-    <CommentDetail author="Sam" />
-    <CommentDetail author="Alex" />
-    <CommentDetail author="Jane" />
+    <CommentDetail author="Sam" timeAgo="Today at 4:43pm" commentContent="Great blog post, dude!" />
+    <CommentDetail author="Alex" timeAgo="Today at 2:45pm" commentContent="Interesting and informative read." />
+    <CommentDetail author="Jane" timeAgo="Yesterday at 5:43am" commentContent="9/11 was an inside job." />
     </div>
   );
 };
